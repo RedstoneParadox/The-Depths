a2ab3d714d97833c9934effc0bca84d5efea00fd
RedstoneParadox <spinjitzu00@gmail.com>
2019-10-16 15:17:58 -0700

    Made some changes to the ceiling of The Depths

--- src/main/java/redstoneparadox/thedepths/world/noise/PitNoiseSampler.kt
+++ src/main/java/redstoneparadox/thedepths/world/noise/PitNoiseSampler.kt
@@ -8,13 +8,20 @@ class PitNoiseSampler(seed: Long, private val cutoff: Double, private val strech
     private val simplexNoise: OpenSimplexNoise = OpenSimplexNoise(seed)
 
     fun isStone(x: Int, y: Int, z: Int): Boolean = when (y) {
-        in 240..254 -> eval(x, z) < calculateOffset(y - 240, 16.0)
-        in 72..239 -> eval(x, z)  < cutoff
+        in 252..254 -> eval(x, z) < calculateTopSlope(calculateOffset(247, 16.0), y - 248)
+        251 -> eval(x, z) < cutoff * 1.25
+        250 -> eval(x, z) < cutoff
+        249 -> eval(x, z) < cutoff * 0.5
+        248 -> eval(x, z) < cutoff * 0.25
+        in 232..247 -> eval(x, z) < calculateOffset(y - 232, 16.0)
+        in 72..231 -> eval(x, z)  < cutoff
         in 64..71 -> eval(x, z)  < cutoff - calculateOffset(y - 64, 8.0)
         else -> false
     }
 
     private fun calculateOffset(value: Int, divisor: Double): Double = (cos((PI * value)/divisor) + 1)/2 * (cutoff/2)
+
+    private fun calculateTopSlope(initial: Double, value: Int): Double = -(value * initial)/4 + value
 
     private fun eval(x: Int, z: Int) = simplexNoise.eval(x.toDouble()/strechFactor, z.toDouble()/strechFactor)
 }
