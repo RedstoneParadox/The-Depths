f1abaf0c72954f9f48eb1e8cabaef5ec73471020
RedstoneParadox <spinjitzu00@gmail.com>
2019-10-17 12:42:58 -0700

    Terrain gen now moved to the surface builder
    Very buggy atm

--- src/main/java/redstoneparadox/thedepths/world/DepthsChunkGenerator.kt
+++ src/main/java/redstoneparadox/thedepths/world/DepthsChunkGenerator.kt
@@ -15,13 +15,6 @@ import redstoneparadox.thedepths.world.noise.PitNoiseSampler
 
 class DepthsChunkGenerator(world: IWorld, biomeSource: BiomeSource, config: DepthsChunkGeneratorConfig): ChunkGenerator<DepthsChunkGeneratorConfig>(world, biomeSource, config) {
 
-    val BEDROCK = Blocks.BEDROCK.defaultState
-    val STONE = DepthsBlocks.DEEP_ROCK.defaultState
-
-    val pitNoiseSampler: PitNoiseSampler = PitNoiseSampler(world.seed, 0.5, 32.0)
-
-    val deepRockNoise: OpenSimplexNoise = OpenSimplexNoise(world.seed)
-
     override fun buildSurface(region: ChunkRegion, chunk: Chunk) {
         //buildBedrock(chunk)
         //buildStone(chunk)
@@ -52,33 +45,4 @@ class DepthsChunkGenerator(world: IWorld, biomeSource: BiomeSource, config: Dept
     override fun getHeightOnGround(x: Int, z: Int, heightMapType: Heightmap.Type): Int = 64
 
     override fun getSpawnHeight(): Int = 64
-
-    private fun buildBedrock(chunk: Chunk) {
-        for (x in 0..15) {
-            for (z in 0..15) {
-                chunk.setBlockState(BlockPos(x, 0, z), BEDROCK, true)
-                chunk.setBlockState(BlockPos(x, 255, z), BEDROCK, true)
-            }
-        }
-    }
-
-    private fun buildStone(chunk: Chunk) {
-        for (x in 0..15) {
-            for (z in 0..15) {
-                val absolutePos = chunk.pos.toBlockPos(x, 0, z)
-                for (y in 0..255) {
-                    if (pitNoiseSampler.isStone(absolutePos.x, y, absolutePos.z)) {
-                        val state = if (sampleDeepRockNoise(absolutePos.x, y, absolutePos.z)) {DepthsBlocks.DEEP_ROCK} else {Blocks.STONE}.defaultState
-                        chunk.setBlockState(BlockPos(x, y, z), state, true)
-                    }
-                }
-            }
-        }
-    }
-
-    private fun sampleDeepRockNoise(x: Int, y: Int, z: Int): Boolean {
-        val stretch = 12.0
-        val cutoff = 0.5
-        return deepRockNoise.eval(x.toDouble()/stretch, y.toDouble()/stretch, z.toDouble()/stretch) < cutoff
-    }
 }
--- src/main/java/redstoneparadox/thedepths/world/gen/surfacebuilder/DepthsSurfaceBuilder.kt
+++ src/main/java/redstoneparadox/thedepths/world/gen/surfacebuilder/DepthsSurfaceBuilder.kt
@@ -2,16 +2,23 @@ package redstoneparadox.thedepths.world.gen.surfacebuilder
 
 import com.mojang.datafixers.Dynamic
 import net.minecraft.block.BlockState
+import net.minecraft.block.Blocks
+import net.minecraft.util.math.BlockPos
 import net.minecraft.world.biome.Biome
 import net.minecraft.world.chunk.Chunk
 import net.minecraft.world.gen.surfacebuilder.SurfaceBuilder
+import redstoneparadox.thedepths.block.DepthsBlocks
+import redstoneparadox.thedepths.world.noise.OpenSimplexNoise
 import redstoneparadox.thedepths.world.noise.PitNoiseSampler
 import java.util.*
 import java.util.function.Function
 
 class DepthsSurfaceBuilder<T: DepthsSurfaceConfig>(function_1: Function<Dynamic<*>, out T>?) : SurfaceBuilder<T>(function_1) {
 
-    var pitNoiseSampler: PitNoiseSampler? = null
+    var pitSampler: PitNoiseSampler? = null
+    private var stoneSampler: OpenSimplexNoise? = null
+
+    val BEDROCK = Blocks.BEDROCK.defaultState
 
     override fun generate(
         random: Random,
@@ -21,16 +28,39 @@ class DepthsSurfaceBuilder<T: DepthsSurfaceConfig>(function_1: Function<Dynamic<
         y: Int,
         z: Int,
         var7: Double,
-        var9: BlockState?,
-        var10: BlockState?,
+        defaultBlock: BlockState?,
+        defaultFluid: BlockState?,
         var11: Int,
-        var12: Long,
+        seed: Long,
         config: T
     ) {
+        val primaryStone = config.underMaterial
+        val secondaryStone = config.secondaryStone
 
+        buildBedrock(chunk, x, y, z)
+        buildStone(chunk, primaryStone, secondaryStone, x, y, z)
     }
 
     override fun initSeed(seed: Long) {
-        pitNoiseSampler = PitNoiseSampler(seed, 0.5, 32.0)
+        if (pitSampler == null) pitSampler = PitNoiseSampler(seed, 0.5, 32.0)
+        if (stoneSampler == null) stoneSampler = OpenSimplexNoise(seed)
+    }
+
+    private fun buildBedrock(chunk: Chunk, x: Int, y: Int, z: Int) {
+        if (y == 0 || y == 255) chunk.setBlockState(BlockPos(x, y, z), BEDROCK, true)
+    }
+
+    private fun buildStone(chunk: Chunk, primary: BlockState, seconadry: BlockState, x: Int, y: Int, z: Int) {
+        val absolutePos = chunk.pos.toBlockPos(x, y, z)
+        if (pitSampler!!.isStone(absolutePos.x, y, absolutePos.z)) {
+            val state = if (sampleDeepRockNoise(absolutePos.x, y, absolutePos.z)) {primary} else {seconadry}
+            chunk.setBlockState(BlockPos(x, y, z), state, true)
+        }
+    }
+
+    private fun sampleDeepRockNoise(x: Int, y: Int, z: Int): Boolean {
+        val stretch = 12.0
+        val cutoff = 0.5
+        return stoneSampler?.eval(x.toDouble()/stretch, y.toDouble()/stretch, z.toDouble()/stretch)!! < cutoff
     }
 }
