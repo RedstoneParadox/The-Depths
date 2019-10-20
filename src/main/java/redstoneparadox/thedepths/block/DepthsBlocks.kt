9fe29096e362a092cdce2870e732f8a4f68ba43b
RedstoneParadox <spinjitzu00@gmail.com>
2019-10-19 20:46:37 -0700

    Added multiple deep rock decorative blocks

--- src/main/java/redstoneparadox/thedepths/block/DepthsBlocks.kt
+++ src/main/java/redstoneparadox/thedepths/block/DepthsBlocks.kt
@@ -1,14 +1,25 @@
 package redstoneparadox.thedepths.block
 
 import net.fabricmc.fabric.api.block.FabricBlockSettings
-import net.minecraft.block.Block
-import net.minecraft.block.Blocks
+import net.minecraft.block.*
 
 object DepthsBlocks: BlocksInitializer() {
 
     val DEEP_ROCK: Block = Block(FabricBlockSettings.copy(Blocks.STONE).build())
+    val DEEP_ROCK_SLAB: SlabBlock = SlabBlock(FabricBlockSettings.copy(Blocks.STONE_SLAB).build())
+    val DEEP_ROCK_STAIRS: StairsBlock = object : StairsBlock(DEEP_ROCK.defaultState, FabricBlockSettings.copy(Blocks.STONE_BRICKS).build()) {}
+    val DEEP_ROCK_BRICKS: Block = Block(FabricBlockSettings.copy(Blocks.STONE_BRICKS).build())
+    val DEEP_ROCK_BRICK_SLAB: SlabBlock = SlabBlock(FabricBlockSettings.copy(Blocks.STONE_BRICK_SLAB).build())
+    val DEEP_ROCK_BRICK_STAIRS: StairsBlock = object : StairsBlock(DEEP_ROCK_BRICKS.defaultState, FabricBlockSettings.copy(Blocks.STONE_BRICKS).build()) {}
+    val DEEP_ROCK_BRICK_WALL: WallBlock = WallBlock(FabricBlockSettings.copy(Blocks.STONE_BRICK_WALL).build())
 
     fun init() {
         register("deep_rock", DEEP_ROCK)
+        register("deep_rock_slab", DEEP_ROCK_SLAB)
+        register("deep_rock_stairs", DEEP_ROCK_STAIRS)
+        register("deep_rock_bricks", DEEP_ROCK_BRICKS)
+        register("deep_rock_brick_slab", DEEP_ROCK_BRICK_SLAB)
+        register("deep_rock_brick_stairs", DEEP_ROCK_BRICK_STAIRS)
+        register("deep_rock_brick_wall", DEEP_ROCK_BRICK_WALL)
     }
 }
--- src/main/java/redstoneparadox/thedepths/items/DepthsItems.kt
+++ src/main/java/redstoneparadox/thedepths/items/DepthsItems.kt
@@ -7,9 +7,21 @@ import redstoneparadox.thedepths.block.DepthsBlocks
 
 object DepthsItems: ItemsInitializer() {
 
-    val DEEP_ROCK: BlockItem = registerNonNullBlockItem("deep_rock", DepthsBlocks.DEEP_ROCK, Item.Settings().group(ItemGroup.BUILDING_BLOCKS))
+    val DEEP_ROCK: BlockItem = BlockItem(DepthsBlocks.DEEP_ROCK, Item.Settings().group(ItemGroup.BUILDING_BLOCKS))
+    val DEEP_ROCK_SLAB: BlockItem = BlockItem(DepthsBlocks.DEEP_ROCK_SLAB, Item.Settings().group(ItemGroup.BUILDING_BLOCKS))
+    val DEEP_ROCK_STAIRS: BlockItem = BlockItem(DepthsBlocks.DEEP_ROCK_STAIRS, Item.Settings().group(ItemGroup.BUILDING_BLOCKS))
+    val DEEP_ROCK_BRICKS: BlockItem = BlockItem(DepthsBlocks.DEEP_ROCK_BRICKS, Item.Settings().group(ItemGroup.BUILDING_BLOCKS))
+    val DEEP_ROCK_BRICK_SLAB: BlockItem = BlockItem(DepthsBlocks.DEEP_ROCK_BRICK_SLAB, Item.Settings().group(ItemGroup.BUILDING_BLOCKS))
+    val DEEP_ROCK_BRICK_STAIRS: BlockItem = BlockItem(DepthsBlocks.DEEP_ROCK_BRICK_STAIRS, Item.Settings().group(ItemGroup.BUILDING_BLOCKS))
+    val DEEP_ROCK_BRICK_WALL: BlockItem = BlockItem(DepthsBlocks.DEEP_ROCK_BRICK_WALL, Item.Settings().group(ItemGroup.DECORATIONS))
 
     fun init() {
-
+        register("deep_rock", DEEP_ROCK)
+        register("deep_rock_slab", DEEP_ROCK_SLAB)
+        register("deep_rock_stairs", DEEP_ROCK_STAIRS)
+        register("deep_rock_bricks", DEEP_ROCK_BRICKS)
+        register("deep_rock_brick_slab", DEEP_ROCK_BRICK_SLAB)
+        register("deep_rock_brick_stairs", DEEP_ROCK_BRICK_STAIRS)
+        register("deep_rock_brick_wall", DEEP_ROCK_BRICK_WALL)
     }
 }
