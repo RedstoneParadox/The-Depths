b7a93bd31718016825ea2db0ed48b2c43ee06945
RedstoneParadox <spinjitzu00@gmail.com>
2020-06-15 09:49:35 -0700

    Revert "Removed int range from DepthsLayer"
    This reverts commit fc0f87a32f8b9ad5c420d8273b4848c6bfb35519.

--- src/main/java/io/github/redstoneparadox/thedepths/world/gen/surfacebuilder/DepthsLayer.kt
+++ src/main/java/io/github/redstoneparadox/thedepths/world/gen/surfacebuilder/DepthsLayer.kt
@@ -4,12 +4,11 @@ import com.mojang.serialization.Codec
 import com.mojang.serialization.codecs.RecordCodecBuilder
 import io.github.redstoneparadox.thedepths.util.Codecs
 
-data class DepthsLayer(val bottom: Int, val top: Int, val holes: Boolean, val connected: Boolean, val filled: Boolean) {
+data class DepthsLayer(val range: IntRange, val holes: Boolean, val connected: Boolean, val filled: Boolean) {
     companion object {
         val CODEC: Codec<DepthsLayer> = RecordCodecBuilder.create { instance ->
             instance.group(
-                Codec.INT.fieldOf("bottom").forGetter { it.bottom },
-                Codec.INT.fieldOf("top").forGetter { it.top },
+                Codecs.INT_RANGE.fieldOf("range").forGetter { it.range },
                 Codec.BOOL.fieldOf("holes").forGetter { it.holes },
                 Codec.BOOL.fieldOf("connected").forGetter { it.connected },
                 Codec.BOOL.fieldOf("filled").forGetter { it.filled }
